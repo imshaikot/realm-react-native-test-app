abf75e3cc5613e0dc54bc83baa15b23806245f2e
Shariar Shaikot <mail2shariar@gmail.com>
2018-05-11 22:04:39 +0600

    fixed some UI issues of List Component

---
+++ src/components/ListItemContainer.js
@@ -0,0 +1,42 @@
+import React, { Fragment } from 'react';
+import { View } from 'react-native';
+
+const styles = {
+  containerStyle: {
+    // borderBottomColor: 'rgb(235, 236, 241)',
+    // borderTopColor: 'transparent',
+    // borderLeftColor: 'transparent',
+    // borderRightColor: 'transparent',
+    // borderBottomWidth: 1,
+    // borderTopWidth: 0,
+    // borderLeftWidth: 0,
+    // borderRightWidth: 0,
+    elevation: 1,
+    marginLeft: 8,
+    marginRight: 8,
+    paddingTop: 12,
+    paddingBottom: 12,
+    flex: 1,
+    flexDirection: 'row',
+  },
+  vBorder: {
+    backgroundColor: 'rgb(235, 236, 241)',
+    height: 1,
+    marginLeft: 12,
+    marginRight: 12,
+  },
+};
+
+const ListItemContainer = (props) => {
+  const { containerStyle, vBorder } = styles;
+  return (
+    <Fragment>
+      <View style={containerStyle}>
+        {props.children}
+      </View>
+      <View style={vBorder} />
+    </Fragment>
+  );
+};
+
+export default ListItemContainer;
